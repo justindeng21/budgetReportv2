2275f2e86c3ba709da8d84caabc5863d6323a7bf
Justin Deng <justin.n.deng@gmail.com>
2023-05-20 23:26:53 -0600

    auto deply

--- budgetReport/src/index.tsx
+++ budgetReport/src/index.tsx
@@ -2,36 +2,62 @@ import './components/styles.css'
 import ReportingToolPage  from './components/reportingtool';
 import LoginPage from './components/login'
 // UNINSTALL import { BrowserRouter, Routes, Route } from "react-router-dom";
-import React from 'react';
+import React, { Component } from 'react';
 
 
 import ReactDOM from 'react-dom/client';
+import { response } from 'express';
 const domain = 'https://budgetreportapi.herokuapp.com'
 
 
-function Controller() {
-    const pathname = window.location.pathname
+
+type ControllerState = {
+    validated : boolean,
+    validToken : boolean
+}
+
+class Controller extends Component<{},ControllerState> {
+    
 
 
-
+    constructor(props:any){
+        super(props)
+        this.state = {validated:false,validToken:false}
+    }
         
-    fetch(domain + '/validateToken', {
-        method: 'GET',
-        credentials: 'include', 
-    }).then((res)=>{
-        if(res['status'] === 200){
-            console.log('hi')
+    validateToken() {
+        fetch(domain + '/validateToken', {
+            method: 'GET',
+            credentials: 'include', 
+
+        }).then((res)=>{
+            if(res['status'] === 200){
+                this.setState({validated:true,validToken:true})
+            }
+            else{
+                this.setState({validated:true,validToken:false})
+            }
+        })
+    
+    }
+     
+   
+    render(): React.ReactNode {
+        if(this.state.validated === false)
+            return null;
+        
+        else if(this.state.validToken === true){
             return(
                 <ReportingToolPage></ReportingToolPage>
             )
         }
-    })  
 
-
-    return(
-        <LoginPage></LoginPage>
-    )
-    
+        else if(this.state.validToken === false){
+            return(
+                <LoginPage></LoginPage>
+            )
+        }
+    }
     
 }
 
