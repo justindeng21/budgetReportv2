edabe913353a23a7218f53abef345a71b47cbdbf
Justin Deng <justin.deng@crownpeak.com>
2023-11-08 09:28:54 -0700

    auto deply

--- budgetReport/src/index.tsx
+++ budgetReport/src/index.tsx
@@ -27,71 +27,71 @@ function Controller() {
 
     
 
-    (function (id:number) : void {
+    // (function (id:number) : void {
 
-        function append(scriptid:string, url:string, async:boolean) : void {
-            var d = document, sn = 'script', f = d.getElementsByTagName(sn)[0];
-            if (!f) f = d.head;
-            var s : any 
-            s = d.createElement(sn);
-            s.async = async;
-            s.id = scriptid;
-            s.src = url;
-            s.charset = 'utf-8';
-            f.parentNode?.insertBefore(s, f);
-        }
+    //     function append(scriptid:string, url:string, async:boolean) : void {
+    //         var d = document, sn = 'script', f = d.getElementsByTagName(sn)[0];
+    //         if (!f) f = d.head;
+    //         var s : any 
+    //         s = d.createElement(sn);
+    //         s.async = async;
+    //         s.id = scriptid;
+    //         s.src = url;
+    //         s.charset = 'utf-8';
+    //         f.parentNode?.insertBefore(s, f);
+    //     }
     
-        function is2parttld(value : string) : boolean {
-            var tldindicators = ['co', 'com', 'info', 'web', 'info', 'gov', 'edu', 'biz', 'net', 'org'];
-            var countryindicators = ['uk', 'us', 'fr', 'es', 'de', 'at', 'au', 'ae', 'be', 'br', 'ca', 'ch', 'cn', 'co', 'cz', 'dk', 'eg', 'eu', 'fi', 'gb', 'gr', 'hk', 'hr', 'hu', 'ie', 'in', 'jp', 'mx', 'nl', 'no', 'nz', 'pl', 'ro', 'ru', 'se'];
-            return (tldindicators.indexOf(value) !== -1 || countryindicators.indexOf(value) !== -1);
-        }
+    //     function is2parttld(value : string) : boolean {
+    //         var tldindicators = ['co', 'com', 'info', 'web', 'info', 'gov', 'edu', 'biz', 'net', 'org'];
+    //         var countryindicators = ['uk', 'us', 'fr', 'es', 'de', 'at', 'au', 'ae', 'be', 'br', 'ca', 'ch', 'cn', 'co', 'cz', 'dk', 'eg', 'eu', 'fi', 'gb', 'gr', 'hk', 'hr', 'hu', 'ie', 'in', 'jp', 'mx', 'nl', 'no', 'nz', 'pl', 'ro', 'ru', 'se'];
+    //         return (tldindicators.indexOf(value) !== -1 || countryindicators.indexOf(value) !== -1);
+    //     }
     
-        function getRootDomain() : string {
-            var parts = window.location.hostname.split('.');
-            if (parts.length === 2) rootDomain = parts[0];
-            else if (parts.length > 2) {
-                // see if the next to last value is a common tld
-                var part = parts[parts.length - 2];
-                if (is2parttld(part)) {
-                    rootDomain = parts[parts.length - 3]; // go back one more
-                }
-                else {
-                    rootDomain = part;
-                }
-            }
+    //     function getRootDomain() : string {
+    //         var parts = window.location.hostname.split('.');
+    //         if (parts.length === 2) rootDomain = parts[0];
+    //         else if (parts.length > 2) {
+    //             // see if the next to last value is a common tld
+    //             var part = parts[parts.length - 2];
+    //             if (is2parttld(part)) {
+    //                 rootDomain = parts[parts.length - 3]; // go back one more
+    //             }
+    //             else {
+    //                 rootDomain = part;
+    //             }
+    //         }
     
-            return rootDomain;
-        }
+    //         return rootDomain;
+    //     }
     
         
-        window.evidon = {};
-        window.evidon.id = id;
-        window.evidon.test = false;  // set to true for non-production testing.
-        //window.evidon.userid = '';
+    //     window.evidon = {};
+    //     window.evidon.id = id;
+    //     window.evidon.test = false;  // set to true for non-production testing.
+    //     //window.evidon.userid = '';
     
-        var cdn = '//c.evidon.com/', rootDomain = getRootDomain(), noticecdn = cdn + 'sitenotice/';
-        append('evidon-notice', noticecdn + 'evidon-sitenotice-tag.js', false);
-        append('evidon-location', cdn + 'geo/country.js', true);
-        append('evidon-themes', noticecdn + id + '/snthemes.js', true);
-        if (rootDomain) append('evidon-settings', noticecdn + id + '/' + rootDomain + (window.evidon.test ? '/test' : '') + '/settingsV2.js', true);
+    //     var cdn = '//c.evidon.com/', rootDomain = getRootDomain(), noticecdn = cdn + 'sitenotice/';
+    //     append('evidon-notice', noticecdn + 'evidon-sitenotice-tag.js', false);
+    //     append('evidon-location', cdn + 'geo/country.js', true);
+    //     append('evidon-themes', noticecdn + id + '/snthemes.js', true);
+    //     if (rootDomain) append('evidon-settings', noticecdn + id + '/' + rootDomain + (window.evidon.test ? '/test' : '') + '/settingsV2.js', true);
     
-        window.evidon.priorConsentCallback = function (categories:object, vendors:object, cookies:object) {
+    //     window.evidon.priorConsentCallback = function (categories:object, vendors:object, cookies:object) {
             
-        }
+    //     }
     
-        window.evidon.closeCallback = function () {
+    //     window.evidon.closeCallback = function () {
 
-        }
+    //     }
     
-        window.evidon.consentWithdrawnCallback = function () {
+    //     window.evidon.consentWithdrawnCallback = function () {
 
-        }
+    //     }
     
-        window.evidon.consentDeclinedCallback = function () {
+    //     window.evidon.consentDeclinedCallback = function () {
 
-        }
-    })(3714);
+    //     }
+    // })(3714);
 
 
 
